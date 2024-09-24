a71d4c5a653f8c9f8397c1a4a3d24a3898da140b
ricardo-deutschendorf <163420542+ricardo-deutschendorf@users.noreply.github.com>
2024-09-24 11:25:39 -0300

    atualizações no front end

--- routes/chat.js
+++ routes/chat.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 const mysql = require("mysql");
 
-
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -17,119 +16,89 @@ con.connect(function (err) {
 
 router.post("/", function (req, res) {
   req.session.amigoid = req.body["idUser"];
-  var amigoid = req.body["idUser"];
+  const amigoid = req.body["idUser"];
   console.log("Amigo: " + amigoid);
-  res.render("chat", {
-    amigoid: amigoid,
-    title: "Chat",
+
+  // Consulta para obter informações do tatuador
+  const sql = "SELECT * FROM usuarios WHERE id = ?";
+  con.query(sql, amigoid, (err, result) => {
+    if (err) throw err;
+
+    // Supondo que você tenha apenas um resultado
+    const tatuador = result[0];
+    res.render("chat", {
+      amigoid: amigoid,
+      title: "Chat",
+      imagem: tatuador.imagem,
+      nome: tatuador.nome_artistico, // Nome do tatuador
+      estilo: tatuador.estilo, // Estilo do tatuador
+      descricao: tatuador.descricao // Descrição do tatuador
+    });
   });
 });
 
 router.post("/recebemensagens", function (req, res) {
-  usuario_logado = req.session.passport.user.id;
-  amigo = req.session.amigoid;
+  const usuario_logado = req.session.passport.user.id;
+  const amigo = req.session.amigoid;
 
-  var sql =
+  const sql =
     "INSERT INTO chats (enviou_id, recebeu_id, mensagem, lida) VALUES ?";
-  var values = [[usuario_logado, amigo, req.body["mensagem"], 0]];
+  const values = [[usuario_logado, amigo, req.body["mensagem"], 0]];
   con.query(sql, [values], function (err, result) {
     if (err) throw err;
-    console.log("Numero de registros inseridos: " + result.affectedRows);
+    console.log("Número de registros inseridos: " + result.affectedRows);
   });
   res.send("Mensagem salva");
 });
 
 router.post("/buscamensagens", function (req, res) {
-  usuario_logado = req.session.passport.user.id;
-  foto_logado = req.session.passport.user.imagem;
-  amigo = req.session.amigoid;
+  const usuario_logado = req.session.passport.user.id;
+  const amigo = req.session.amigoid;
   console.log("Amigo: " + amigo);
-  retorno = "";
-  var sql = "SELECT * FROM usuarios where id= ? ORDER BY id;";
-  con.query(sql, amigo, function (err, result, fields) {
+  let retorno = "";
+  
+  const sql = "SELECT * FROM usuarios WHERE id = ?";
+  con.query(sql, amigo, function (err, result) {
     if (err) throw err;
-    console.log(result[0]["nome"]);
-    foto_amigo = result[0]["imagem"];
-    valores = [usuario_logado, amigo, amigo, usuario_logado];
+    
+    const foto_amigo = result[0]["imagem"];
+    const valores = [usuario_logado, amigo, amigo, usuario_logado];
 
-    //sql5 = "SELECT COUNT(id) FROM `chats` WHERE lida=0 && recebeu_id=?"
-    //valores = [usuario_logado];
+    const sql2 =
+      "SELECT * FROM chats WHERE (enviou_id = ? AND recebeu_id = ?) OR (enviou_id = ? AND recebeu_id = ?) ORDER BY id LIMIT 500;";
+    
+    con.query(sql2, valores, function (err, mensagens) {
+      if (err) throw err;
 
-    sql2 =
-      "SELECT * FROM chats WHERE (enviou_id=? && recebeu_id= ?) or (enviou_id=? && recebeu_id= ?) ORDER BY id  LIMIT 500;";
-
-    con.query(sql2, valores, function (err, mensagens, fields) {
       if (mensagens.length > 0) {
-        sql3 = "UPDATE chats set lida = 1 where id<=? && recebeu_id= ?";
-        valores = [mensagens[mensagens.length - 1]["id"], usuario_logado];
-        con.query(sql3, valores, function (err, mensagens, fields) {
+        const sql3 = "UPDATE chats SET lida = 1 WHERE id <= ? AND recebeu_id = ?";
+        const valores = [mensagens[mensagens.length - 1]["id"], usuario_logado];
+        con.query(sql3, valores, function (err) {
           if (err) throw err;
         });
       } else {
         console.log("Não há mensagens disponíveis");
       }
-      if (err) throw err;
+
       mensagens.forEach(function (dados) {
         if (usuario_logado == dados["enviou_id"]) {
-          if (req.session.passport.user.imagem.search("https")) {
-            retorno =
-              retorno +
-              "<div class='media media-chat media-chat-reverse'>" +
-              "<img class='avatar' src=imagens/" +
-              foto_logado +
-              ">" +
-              "<div class='media-body'>" +
-              "<p>" +
-              dados["mensagem"] +
-              "</p>" +
-              "</div>" +
-              "</div>" +
-              "<div class='media media-meta-day'> </div>";
-          } else {
-            retorno =
-              retorno +
-              "<div class='media media-chat media-chat-reverse'>" +
-              "<img class='avatar' src=" +
-              foto_logado +
-              ">" +
-              "<div class='media-body'>" +
-              "<p>" +
-              dados["mensagem"] +
-              "</p>" +
-              "</div>" +
-              "</div>" +
-              "<div class='media media-meta-day'> </div>";
-          }
+          retorno += `
+            <div class='media media-chat media-chat-reverse'>
+              <img class='avatar' src=${foto_logado}>
+              <div class='media-body'>
+                <p>${dados["mensagem"]}</p>
+              </div>
+            </div>
+            <div class='media media-meta-day'></div>`;
         } else {
-          if (req.session.passport.user.imagem.search("https")) {
-            retorno =
-              retorno +
-              "<div class='media media-chat'>" +
-              "<img class='avatar' src=imagens/" +
-              foto_amigo +
-              ">" +
-              "<div class='media-body'>" +
-              "<p>" +
-              dados["mensagem"] +
-              "</p>" +
-              "</div>" +
-              "</div>" +
-              "<div class='media media-meta-day'> </div>";
-          } else {
-            retorno =
-              retorno +
-              "<div class='media media-chat'>" +
-              "<img class='avatar' src=" +
-              foto_amigo +
-              ">" +
-              "<div class='media-body'>" +
-              "<p>" +
-              dados["mensagem"] +
-              "</p>" +
-              "</div>" +
-              "</div>" +
-              "<div class='media media-meta-day'> </div>";
-          }
+          retorno += `
+            <div class='media media-chat'>
+              <img class='avatar' src=${foto_amigo}>
+              <div class='media-body'>
+                <p>${dados["mensagem"]}</p>
+              </div>
+            </div>
+            <div class='media media-meta-day'></div>`;
         }
       });
       res.send(JSON.stringify(retorno));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var router = express.Router();
+var Tatuador = require("../models/Usuario"); // Certifique-se de que o caminho está correto
 
 router.get("/", function (req, res, next) {
-  res.render("menu", { title: "Express"});
+  Tatuador.findAll({ where: { tipo: "tatuador" } }) // Busca todos os tatuadores
+    .then((tatuadores) => {
+      res.render("menu", { title: "Express", tatuadores }); // Passa tatuadores para o EJS
+    })
+    .catch((err) => {
+      console.error(err); // Log de erro
+      res.render("menu", { title: "Express", tatuadores: [] }); // Passa um array vazio em caso de erro
+    });
 });
 
 module.exports = router;
