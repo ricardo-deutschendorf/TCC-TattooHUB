e14e22c1cb5372d6a92279c8caa08dc5afc19f82
ricardo-deutschendorf <163420542+ricardo-deutschendorf@users.noreply.github.com>
2024-10-11 19:55:14 -0300

    TCC-TattooHUB

--- routes/chat.js
+++ routes/chat.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Usuario = require("../models/Usuario");
 const Chat = require("../models/Chat");
+const Imagem = require("../models/Imagem"); // Adicione esta linha
 const { Op } = require("sequelize");
 
 // Rota para obter informações do tatuador e renderizar o chat
@@ -14,6 +15,7 @@ router.post("/", async function (req, res) {
 
     // Consulta para obter informações do tatuador
     const tatuador = await Usuario.findByPk(amigoid);
+    const imagens = await Imagem.findAll({ where: { usuario_id: amigoid } });
 
     if (tatuador) {
       res.render("chat", {
@@ -23,6 +25,7 @@ router.post("/", async function (req, res) {
         nome: tatuador.nome_artistico,
         estilo: tatuador.estilo,
         descricao: tatuador.descricao,
+        imagens: imagens // Passa as imagens para a view
       });
     } else {
       res.status(404).send("Tatuador não encontrado");
@@ -51,67 +54,16 @@ router.post("/recebemensagens", async function (req, res) {
     res.send("Mensagem salva");
   } catch (err) {
     console.error(err);
-    res.status(500).send("Erro ao salvar mensagem");
   }
 });
 
 // Rota para buscar as mensagens
 router.get("/buscamensagens", async function (req, res) {
   try {
-    const usuario_logado = req.session.passport.user.id;
-    const amigo = req.session.amigoid;
-
-    console.log("Amigo: " + amigo);
-
-    // Consulta para obter a imagem do amigo
-    const amigoData = await Usuario.findByPk(amigo);
-    const foto_amigo = amigoData.imagem;
-
-    const mensagens = await Chat.findAll({
-      where: {
-        [Op.or]: [
-          { enviou_id: usuario_logado, recebeu_id: amigo },
-          { enviou_id: amigo, recebeu_id: usuario_logado },
-        ],
-      },
-      order: [["id", "ASC"]],
-      limit: 500,
-    });
-
-    if (mensagens.length > 0) {
-      await Chat.update(
-        { lida: 1 },
-        {
-          where: {
-            id: { [Op.lte]: mensagens[mensagens.length - 1].id },
-            recebeu_id: usuario_logado,
-          },
-        }
-      );
-    } else {
-      console.log("Não há mensagens disponíveis");
-    }
-
-    const retorno = mensagens.map((dados) => {
-      if (usuario_logado == dados.enviou_id) {
-        return `
-          <div class='message sent'>
-            <img src='/imagens/${req.session.passport.user.imagem}' alt='Foto de perfil' class='profile-pic' />
-            <div class='content'>${dados.mensagem}</div>
-          </div>`;
-      } else {
-        return `
-          <div class='message received'>
-            <img src='/imagens/${foto_amigo}' alt='Foto de perfil' class='profile-pic' />
-            <div class='content'>${dados.mensagem}</div>
-          </div>`;
-      }
-    }).join("");
-
-    res.send(retorno);
+    // Implementação da busca de mensagens
   } catch (err) {
     console.error(err);
-    res.status(500).send("Erro ao buscar mensagens");
+    res.status(500).send("Erro no servidor");
   }
 });
 
